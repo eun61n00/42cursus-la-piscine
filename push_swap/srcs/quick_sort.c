2abe6a5cc3097c3f5e75939c0b30d1ea3d03ad5c
eun61n00 <eunbin0624@seoultech.ac.kr>
2022-09-12 23:56:37 +0900

    quick sort

---
+++ push_swap/srcs/quick_sort.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quick_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eukwon <eukwon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 09:50:28 by eukwon            #+#    #+#             */
+/*   Updated: 2022/09/12 10:39:51 by eukwon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	A_to_B(int r, t_double_linked_list **a, t_double_linked_list **b)
+{
+	int	pivot;
+	int	ra_cnt, int_pb_cnt;
+
+	pivot = (*a)->head->data;
+	while (r > 0)
+	{
+		if ((*a)->head->data > pivot)
+		{
+			ra(a);
+		}
+		else
+		{
+			pb(a, b);
+		}
+	}
+}
+
+void 	B_to_A(int r, t_double_linked_list **a, t_double_linked_list **b)
+{
+
+}
+
+void	sort(t_double_linked_list **a, t_double_linked_list **b)
+{
+	int ra_cnt, pb_cnt;
+	while ((*b)->size <= 0)
+	{
+		A_to_B();
+		B_to_A();
+	}
+
+void	quick_sort(int *num_arr, int L, int R)
+{
+	int left = L, right = R;
+	int pivot = num_arr[(L + R) / 2];
+
+	while (left <= right)
+	{
+		while (num_arr[left] < pivot)
+			left++;
+		while (num_arr[right] > pivot)
+			right--;
+		if (left <= right)
+		{
+			swap(num_arr, left, right);
+			left++;
+			right--;
+		}
+		if (L < right) //아직 끝까지 정렬하지 못함
+			quick_sort(num_arr, L, right);
+		if (left < R)
+			quick_sort(num_arr, left, R);
+	}
+}
