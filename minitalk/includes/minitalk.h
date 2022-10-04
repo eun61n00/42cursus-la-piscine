88c0abbeba2d34b3664d772786da4518177d1861
eun61n00 <eunbin0624@seoultech.ac.kr>
2022-10-05 04:09:49 +0900

    edit makefile and headerfile to delete ft_printf archive

--- minitalk/includes/minitalk.h
+++ minitalk/includes/minitalk.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eukwon <eukwon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: eukwon <eukwon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 17:08:44 by eukwon            #+#    #+#             */
-/*   Updated: 2022/10/05 00:13:01 by eukwon           ###   ########.fr       */
+/*   Updated: 2022/10/05 04:08:11 by eukwon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include <signal.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include "../lib/ft_printf/includes/ft_printf.h"
 # include "../lib/libft/includes/libft.h"
 
 #endif
